7354f84919eb57918433fda15467bdbbf5c5d5d9
German Krauss <german@devartis.com>
2011-09-01 09:32:38 -0300

    Export to CVS functionality added (just to test it, not really necessary)

--- web/admin.py
+++ web/admin.py
@@ -2,6 +2,30 @@ from django.contrib import admin
 from web.models import Event
 from web.models import Quote
 
+import csv
+from django.core.exceptions import PermissionDenied
+from django.http import HttpResponse
+
+def export_as_csv(modeladmin, request, queryset):
+    """
+    Generic csv export admin action.
+    """
+    if not request.user.is_staff:
+        raise PermissionDenied
+    opts = modeladmin.model._meta
+    response = HttpResponse(mimetype='text/csv')
+    response['Content-Disposition'] = 'attachment; filename=%s.csv' % unicode(opts).replace('.', '_')
+    writer = csv.writer(response)
+    field_names = [field.name for field in opts.fields]
+    # Write a first row with header information
+    writer.writerow(field_names)
+    # Write data rows
+    for obj in queryset:
+        writer.writerow([getattr(obj, field) for field in field_names])
+    return response
+
+export_as_csv.short_description = "Export selected objects as csv file"
+
 class QuoteInline(admin.TabularInline):
     model = Quote
     fields = ['quote']
@@ -10,7 +34,7 @@ class EventAdmin(admin.ModelAdmin):
     inlines = [
         QuoteInline,
     ]
+    actions = [export_as_csv]
 
 admin.site.register(Event, EventAdmin)
 admin.site.register(Quote)
-
